88d396788a80f95e2ec2974473a4ebd3cb45537e
orangegrove_1955 <orangegrove_1955@hotmail.com>
2019-08-27 14:09:47 +1000

    Added styling and ability to play sound when tapped

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Javascript Music App
 
+This project uses vanilal javascript and CSS to create a simple music app that works based on tapping/clicking one of the pads at the bottom of the screen. Doing so also creates a small visualisation while the sound plays. 
+
 # What I learned
--
+- HTML `audio` tag
+- Flexbox wrapping
+- Flex-grow
 
 # Demo
 A live demo can be viewed [here](#)
--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Working");
+window.addEventListener('load', () => {
+    // Get all sounds from page
+    const sounds = document.querySelectorAll('.sound'),
+    // Get all pads from page
+    pads = document.querySelectorAll('.pads div');
+
+    pads.forEach((pad, index) => {
+        pad.addEventListener('click', function () {
+            // Play sound when clicked on
+            sounds[index].play();
+        })
+    })
+});
